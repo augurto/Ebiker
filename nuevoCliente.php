0d590ca68efc96c4f73a80aeea86328f98468882
Ego <augurto.17@gmail.com>
2023-08-11 10:24:29 -0500

    tipoUsuario

--- nuevoCliente.php
+++ nuevoCliente.php
@@ -225,9 +225,9 @@ if ($tipoUsuario == 1) {
                                                 // Realizar la consulta a la base de datos para obtener los datos de la tabla
                                                 if ($tipoUsuario == 1) {
                                                     // Verificar el valor de $idUsuarioSesion para seleccionar la consulta adecuada
-                                                    if ($idUsuarioSesion == 2) {
+                                                    if ($tipoUsuario == 2) {
                                                         $query2 = "SELECT * FROM fuente where idAterrizajeFuente = 1";
-                                                    } elseif ($idUsuarioSesion == 1) {
+                                                    } elseif ($tipoUsuario == 1) {
                                                         $query2 = "SELECT * FROM fuente where id_fuente in (4,5,6)";
                                                     } else {
                                                         // Otro caso o valor de $idUsuarioSesion
