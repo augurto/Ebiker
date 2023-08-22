4c0b96c7b1b9e86a06d3de86c934f5009407f5d0
Ego <augurto.17@gmail.com>
2023-08-22 09:22:59 -0500

    agregar fila y columna

--- cuadrado.php
+++ cuadrado.php
@@ -10,7 +10,6 @@
             background-color: transparent;
             border: 2px solid blue;
             display: grid;
-            grid-template-columns: repeat(4, 1fr);
             grid-gap: 2px;
         }
 
@@ -21,6 +20,7 @@
 
         .cuadradito.rojo {
             border-color: red;
+            background-color: red;
         }
 
         input {
@@ -29,6 +29,12 @@
     </style>
 </head>
 <body>
+    <label for="filas">Filas:</label>
+    <input type="number" id="filas" />
+    
+    <label for="columnas">Columnas:</label>
+    <input type="number" id="columnas" />
+
     <div class="cuadrado-grande" id="cuadrado">
         <?php
             $numCuadraditos = 16;
@@ -44,12 +50,38 @@
     <script>
         const cuadradoDiv = document.getElementById('cuadrado');
         const cuadraditos = cuadradoDiv.querySelectorAll('.cuadradito');
+        const filasInput = document.getElementById('filas');
+        const columnasInput = document.getElementById('columnas');
 
-        cuadraditos.forEach(cuadradito => {
-            cuadradito.addEventListener('click', () => {
-                cuadradito.classList.toggle('rojo');
+        filasInput.addEventListener('input', actualizarCuadraditos);
+        columnasInput.addEventListener('input', actualizarCuadraditos);
+
+        function actualizarCuadraditos() {
+            const filas = parseInt(filasInput.value) || 0;
+            const columnas = parseInt(columnasInput.value) || 0;
+            const numCuadraditosRojo = 12;
+            const totalCuadraditos = filas * columnas;
+
+            cuadradoDiv.style.gridTemplateColumns = `repeat(${columnas}, 1fr)`;
+            cuadradoDiv.style.gridTemplateRows = `repeat(${filas}, 1fr)`;
+
+            cuadraditos.forEach((cuadradito, index) => {
+                const clase = (index < numCuadraditosRojo) ? 'rojo' : '';
+                cuadradito.className = `cuadradito ${clase}`;
             });
-        });
+
+            if (totalCuadraditos > cuadraditos.length) {
+                for (let i = cuadraditos.length; i < totalCuadraditos; i++) {
+                    const nuevoCuadradito = document.createElement('div');
+                    nuevoCuadradito.className = 'cuadradito';
+                    cuadradoDiv.appendChild(nuevoCuadradito);
+                }
+            } else if (totalCuadraditos < cuadraditos.length) {
+                while (cuadraditos.length > totalCuadraditos) {
+                    cuadraditos[cuadraditos.length - 1].remove();
+                }
+            }
+        }
     </script>
 </body>
 </html>
