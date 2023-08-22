ecb2b4d41229d6843b7edcca3207593c106b582f
Ego <augurto.17@gmail.com>
2023-08-21 19:01:57 -0500

    anterior

--- cuadrado.php
+++ cuadrado.php
@@ -5,9 +5,13 @@
     <style>
         .cuadrado-grande {
             position: relative;
+            width: 300px;
+            height: 300px;
             background-color: transparent;
             border: 2px solid blue;
             display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            grid-gap: 2px;
         }
 
         .cuadradito {
@@ -17,7 +21,6 @@
 
         .cuadradito.rojo {
             border-color: red;
-            background-color: red;
         }
 
         input {
@@ -26,17 +29,12 @@
     </style>
 </head>
 <body>
-    <label for="ancho">Ancho:</label>
-    <input type="number" id="ancho" />
-    
-    <label for="alto">Alto:</label>
-    <input type="number" id="alto" />
-
     <div class="cuadrado-grande" id="cuadrado">
         <?php
+            $numCuadraditos = 16;
             $numCuadraditosRojo = 12;
 
-            for ($i = 1; $i <= ($ancho * $alto); $i++) {
+            for ($i = 1; $i <= $numCuadraditos; $i++) {
                 $clase = ($i <= $numCuadraditosRojo) ? 'rojo' : '';
                 echo '<div class="cuadradito ' . $clase . '"></div>';
             }
@@ -46,24 +44,12 @@
     <script>
         const cuadradoDiv = document.getElementById('cuadrado');
         const cuadraditos = cuadradoDiv.querySelectorAll('.cuadradito');
-        const anchoInput = document.getElementById('ancho');
-        const altoInput = document.getElementById('alto');
 
-        anchoInput.addEventListener('input', actualizarCuadrado);
-        altoInput.addEventListener('input', actualizarCuadrado);
-
-        function actualizarCuadrado() {
-            const ancho = parseFloat(anchoInput.value) || 0;
-            const alto = parseFloat(altoInput.value) || 0;
-            const numCuadraditosRojo = 12;
-
-            cuadradoDiv.style.gridTemplateColumns = `repeat(${ancho}, 1fr)`;
-
-            cuadraditos.forEach((cuadradito, index) => {
-                const clase = (index < numCuadraditosRojo) ? 'rojo' : '';
-                cuadradito.className = `cuadradito ${clase}`;
+        cuadraditos.forEach(cuadradito => {
+            cuadradito.addEventListener('click', () => {
+                cuadradito.classList.toggle('rojo');
             });
-        }
+        });
     </script>
 </body>
 </html>
