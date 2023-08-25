5a318ec4bbf23d94891c52eb9bda38e52355b340
Ego <augurto.17@gmail.com>
2023-08-25 11:06:11 -0500

    aer con condicional

--- clienteNuevo.php
+++ clienteNuevo.php
@@ -214,38 +214,36 @@ $tipoUsuario = $_SESSION['tipoUsuario'];
                                             
                                             <div class="mb-12">
                                                 <label class="form-label">Fuente</label>
-                                                
                                                 <select class="form-control select2" id="prospecto" name="prospecto">
                                                 <?php
-                                             
-                                                include 'includes/conexion.php'; 
-                                                // Realizar la consulta a la base de datos para obtener los datos de la tabla
-                                                $query2 = "SELECT * FROM fuente where id_fuente in(4,5,6)";
-                                                $result2 = mysqli_query($con, $query2);
+                                                    include 'includes/conexion.php'; 
+                                                    // Realizar la consulta a la base de datos para obtener los datos de la tabla
+                                                    $query2 = "SELECT * FROM fuente WHERE id_fuente IN (4,5,6)";
+                                                    $result2 = mysqli_query($con, $query2);
 
-                                                // Verificar si se encontraron resultados
-                                                if (mysqli_num_rows($result2) > 0) {
-                                                    // Generar las opciones dentro del select
-                                                    while ($row2 = mysqli_fetch_assoc($result2)) {
-                                                    $value2 = $row2['tipoFuente'];
-                                                    $text2 = $row2['descripcionFuente'];
-                                                    echo "<option value='" . $value2 . "'>" . $text2 . "</option>";
+                                                    // Verificar si se encontraron resultados
+                                                    if (mysqli_num_rows($result2) > 0) {
+                                                        // Generar las opciones dentro del select
+                                                        while ($row2 = mysqli_fetch_assoc($result2)) {
+                                                            $value2 = $row2['tipoFuente'];
+                                                            $text2 = $row2['descripcionFuente'];
+                                                            echo "<option value='" . $value2 . "'>" . $text2 . "</option>";
+                                                        }
                                                     }
-                                                }
 
-                                                // Cerrar la conexión a la base de datos
-                                                mysqli_close($con);
+                                                    // Cerrar la conexión a la base de datos
+                                                    mysqli_close($con);
                                                 ?>
                                                 </select>
-
                                             </div>
 
-                                            <div class="mb-12">
-                                                <label class="form-label">Campaña</label>
+                                            <?php
+                                            if ($value2 === 6) {
+                                                echo '<div class="mb-12">
+                                                        <label class="form-label">Campaña</label>
+                                                        <select class="form-control select2" id="campana" name="campana">';
                                                 
-                                                <select class="form-control select2" id="campana" name="campana">
-                                                <?php
-                                                 include 'includes/conexion.php'; 
+                                                include 'includes/conexion.php'; 
                                                 // Realizar la consulta a la base de datos para obtener los datos de la tabla
                                                 $queryCamp = "SELECT * FROM campaign";
                                                 $resultCamp = mysqli_query($con, $queryCamp);
@@ -254,18 +252,19 @@ $tipoUsuario = $_SESSION['tipoUsuario'];
                                                 if (mysqli_num_rows($resultCamp) > 0) {
                                                     // Generar las opciones dentro del select
                                                     while ($rowCamp = mysqli_fetch_assoc($resultCamp)) {
-                                                    $valueCamp = $rowCamp['valorCampaign'];
-                                                    $textCamp = $rowCamp['nombreCampaign'];
-                                                    echo "<option value='" . $valueCamp . "'>" . $textCamp . "</option>";
+                                                        $valueCamp = $rowCamp['valorCampaign'];
+                                                        $textCamp = $rowCamp['nombreCampaign'];
+                                                        echo "<option value='" . $valueCamp . "'>" . $textCamp . "</option>";
                                                     }
                                                 }
 
                                                 // Cerrar la conexión a la base de datos
                                                 mysqli_close($con);
-                                                ?>
-                                                </select>
 
-                                            </div>
+                                                echo '</select></div>';
+                                            }
+                                            ?>
+
                                             
 
                                             <div class="mt-6">
