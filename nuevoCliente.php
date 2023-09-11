71bb80965a74015e2cd949e36cf44196e28d4640
Ego <augurto.17@gmail.com>
2023-09-11 11:17:53 -0500

    autoasignacion cambiada

--- nuevoCliente.php
+++ nuevoCliente.php
@@ -154,7 +154,7 @@ if ($tipoUsuario == 1) {
                                             // Verificar el valor de $tipoUsuario
                                             if ($tipoUsuario == 2) {
                                                 // Inicializar un array con los valores permitidos
-                                                $valoresPermitidos = array(5, 6, 4, 3);
+                                                $valoresPermitidos = array(6, 4);
 
                                                 // Realizar una búsqueda en el array para verificar si el último valor es uno de los permitidos
                                                 if (isset($_SESSION['ultimaValorRandom']) && in_array($_SESSION['ultimaValorRandom'], $valoresPermitidos)) {
