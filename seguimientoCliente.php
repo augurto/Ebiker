f46852c4e22a4c980ad2f3bf0712dffd8bc752d3
Ego <augurto.17@gmail.com>
2023-09-11 17:32:43 -0500

    || ($tipoUsuario == 2)

--- seguimientoCliente.php
+++ seguimientoCliente.php
@@ -180,12 +180,9 @@ $dni = $_SESSION['dni'];
                 <!-- end page title -->
 
                 <?php
-                if ($randomUser != $idUsuarioSesion ) {
+                if ($randomUser != $idUsuarioSesion || ($tipoUsuario == 2)) {
                     // El contenido del div no se mostrará si $randomUser es diferente de $idUsuarioSesion
                     echo '<div class="row" style="display:none">';
-                }elseif($tipoUsuario=2){
-                    echo '<div class="row">';
-
                 } else {
                     echo '<div class="row">';
                 }
