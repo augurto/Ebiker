2187042871971002c62e76c3bea3ea7314095651
Ego <augurto.17@gmail.com>
2023-11-13 18:36:55 -0500

    aer

--- evaluacionCrediticia.php
+++ evaluacionCrediticia.php
@@ -257,6 +257,20 @@ if ($tipoUsuario == 1) {
                                     <!-- end row -->
                                     
                                     <div>
+                                    <form action="#" class="dropzone">
+                                        <div class="fallback">
+                                            <input name="file" type="file" multiple="multiple">
+                                        </div>
+                                        <div class="dz-message needsclick">
+                                            <div class="mb-3">
+                                                <i class="display-4 text-muted ri-upload-cloud-2-line"></i>
+                                            </div>
+                                            <h4>Drop files here or click to upload.</h4>
+                                        </div>
+                                    </form>
+                                    <!-- end form -->
+                                </div>
+                                    <div>
                                         <button class="btn btn-primary" type="submit">Actualizar</button>
                                     </div>
                                 </form>
