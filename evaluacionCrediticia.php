7e1eafabf39265eed2b2080a8bd04b81bdfbc945
Ego <augurto.17@gmail.com>
2023-11-13 18:54:18 -0500

    aer

--- evaluacionCrediticia.php
+++ evaluacionCrediticia.php
@@ -257,20 +257,43 @@ if ($tipoUsuario == 1) {
 
                                     <!-- end row -->
 
-                                    <div>
-                                        <form action="#" class="dropzone">
-                                            <div class="fallback">
-                                                <input name="file" type="file" multiple="multiple">
-                                            </div>
-                                            <div class="dz-message needsclick">
-                                                <div class="mb-3">
-                                                    <i class="display-4 text-muted ri-upload-cloud-2-line"></i>
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="card">
+                                                <div class="card-body">
+
+                                                    <h4 class="card-title">Dropzone</h4>
+                                                    <p class="card-title-desc">DropzoneJS is an open source library
+                                                        that provides drag’n’drop file uploads with image previews.
+                                                    </p>
+
+                                                    <div>
+                                                        <form action="#" class="dropzone">
+                                                            <div class="fallback">
+                                                                <input name="file" type="file" multiple="multiple">
+                                                            </div>
+                                                            <div class="dz-message needsclick">
+                                                                <div class="mb-3">
+                                                                    <i class="display-4 text-muted ri-upload-cloud-2-line"></i>
+                                                                </div>
+                                                                <h4>Drop files here or click to upload.</h4>
+                                                            </div>
+                                                        </form>
+                                                        <!-- end form -->
+                                                    </div>
+
+                                                    <div class="text-center mt-4">
+                                                        <button type="button" class="btn btn-primary waves-effect waves-light">Send
+                                                            Files</button>
+                                                    </div>
                                                 </div>
-                                                <h4>Drop files here or click to upload.</h4>
+                                                <!-- end cardbody -->
                                             </div>
-                                        </form>
-                                        <!-- end form -->
+                                            <!-- end card -->
+                                        </div>
+                                        <!-- end col -->
                                     </div>
+                                    <!-- end row -->
                                     <div>
                                         <button class="btn btn-primary" type="submit">Actualizar</button>
                                     </div>
