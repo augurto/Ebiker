fa8b26fa73c9395d1b4d37b144d6894b1e30e436
Ego <augurto.17@gmail.com>
2023-11-17 10:21:27 -0500

    cambio de contraseña bd

--- inicio.php
+++ inicio.php
@@ -135,9 +135,9 @@ if ($tipoUsuario == 1) {
                                                 <?php
                                                 // Establecer la conexión con la base de datos
                                                 $servername = "localhost";
-                                                $username = "u291982824_ebiker";
+                                                $username = "u291982824_ebiker2";
                                                 $password = "21.17.Ebiker";
-                                                $dbname = "u291982824_ebiker";
+                                                $dbname = "u291982824_ebiker2";
 
                                                 $conn = new mysqli($servername, $username, $password, $dbname);
 
--- vendidos.php
+++ vendidos.php
@@ -121,9 +121,9 @@ $empresaUser =$_SESSION['empresaUser'] ;
                                                 <?php
                                                 // Establecer la conexión con la base de datos
                                                 $servername = "localhost";
-                                                $username = "u291982824_ebiker";
+                                                $username = "u291982824_ebiker2";
                                                 $password = "21.17.Ebiker";
-                                                $dbname = "u291982824_ebiker";
+                                                $dbname = "u291982824_ebiker2";
 
                                                 $conn = new mysqli($servername, $username, $password, $dbname);
 
